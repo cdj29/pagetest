d71a8e402b0131dad489f497f8f94e3765dd1a30
cdj29 <568538310@qq.com>
2020-07-06 18:39:28 +0800

    Updates

---
+++ precache-manifest.8148d60fbdc6625bea7de4481df2e2a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b1d99b09e389c93d7b030727fe1037",
+    "url": "/pagetest/index.html"
+  },
+  {
+    "revision": "74119dfe778a8bb86012",
+    "url": "/pagetest/static/css/main.61afb330.chunk.css"
+  },
+  {
+    "revision": "6613a47a4a82a547aae0",
+    "url": "/pagetest/static/js/2.a5597da9.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/pagetest/static/js/2.a5597da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74119dfe778a8bb86012",
+    "url": "/pagetest/static/js/main.bd0973fc.chunk.js"
+  },
+  {
+    "revision": "7b7a8c91214fe7de8bdd",
+    "url": "/pagetest/static/js/runtime-main.802ea62c.js"
+  },
+  {
+    "revision": "1b992319e2d525cb67d6637996663716",
+    "url": "/pagetest/static/media/matcap.1b992319.jpg"
+  }
+]);
